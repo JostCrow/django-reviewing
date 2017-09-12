6106f754fa428a74245941d8c73181940e570edc
Jorik Kraaikamp <jorik.kraaikamp@maykinmedia.nl>
2017-09-12 12:30:23 +0200

    increased version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-reviewing',
-    version='0.0.4',
+    version='0.0.5',
     license='BSD',
 
     # packaging
